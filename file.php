c1789fa14339e1b89ddad3105af8e84ba6d6bbcf
eisa2609 <aisyasaidon@gmail.com>
2024-05-13 18:21:49 +0800

    aisya edit

--- file.php
+++ file.php
@@ -1,58 +1,56 @@
-<html><body style="background-color:powderblue;">
-    <head><title>PBU</title></head>
-
-    <center>
-    <h1>Student Representative Council Nomination</h1>
-    
 <?php
+include 'connection.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["Username"];
+    $password = $_POST["Password"];
 
-    if (isset($_POST["Username"]) && isset($_POST["Password"])) {
-        $username = $_POST["Username"];
-        $password = $_POST["Password"];
+    // Query the database
+    $sql = "SELECT * FROM user_login WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($con, $sql);
 
-        // Defina username and password for officer and candidate
-        $officer_username = "officer";
-        $officer_password = "officer123";
-        $candidate_username = "candidate";
-        $candidate_pasword = "candidate123";
-
-        // Check if entered valid username and password for officer 
-        if ($username === $officer_username && $password === $officer_password) {
+    if ($result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+     
+        if ($username == 'officer1' && $password == 'officer123') {
             header("Location: officer.php");
             exit();
-        } 
-        // Check if entered valid username and password for student 
-        elseif ($username === $candidate_username && $password === $candidate_pasword) {
+        } elseif ($username == 'candidate1' && $password == 'candidate123') {
             header("Location: candidate.php");
             exit();
-        } else {
-            echo '<script>alert("Invalid username or password");</script>';
         }
     }
+
+    // Invalid username or password
+    echo '<script>alert("Invalid username or password");</script>';
 }
 ?>
 
-<form method="post">
-    <table>
-        <tr>
-            <td colspan="2"><h3>--------Log in here--------</h3></td>
-        </tr>
-        <tr> 
-            <td><label for="Username">Username:</label></td>
-            <td><input type="text" id="Username" name="Username"></td>
-        </tr>
-        <tr> 
-            <td><label for="Password">Password:</label></td>
-            <td><input type="password" id="Password" name="Password"></td>
-        </tr>
-        <tr>
-            <td colspan="2"><button type="submit" name="btnCandidate">Login</button></td>
-        </tr>
-    </table>
-</form>
 
-
+<html>
+<body style="background-color:powderblue;">
+<head><title>PBU</title></head>
+<center>
+    <h1>Student Representative Council Nomination</h1>
+    
+    <form method="post">
+        <table>
+            <tr>
+                <td colspan="2"><h3>--------Log in here--------</h3></td>
+            </tr>
+            <tr> 
+                <td><label for="Username">Username:</label></td>
+                <td><input type="text" id="Username" name="Username"></td>
+            </tr>
+            <tr> 
+                <td><label for="Password">Password:</label></td>
+                <td><input type="password" id="Password" name="Password"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><button type="submit" name="btnLogin">Login</button></td>
+            </tr>
+        </table>
+    </form>
 </center>
 </body>
 </html>
