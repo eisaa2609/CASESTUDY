7b09e762b17ca9adb48a2c13daeaa55c18b19dce
Naj <153092631+najwarizal@users.noreply.github.com>
2024-05-16 16:40:18 +0800

    officer list najwa

--- officer_list.php
+++ officer_list.php
@@ -1,11 +1,21 @@
 <?php 
     include('connection.php');
 
-    // SQL query to select data from database
-    $sql = " SELECT name, icNo, regNo, phoneNo, program, jabatan, date FROM candidates";
-    $result = mysqli_query($con, $sql);
+    if (isset($_POST['btnPadam'])) {
+        $name = mysqli_real_escape_string($con, $_POST['search-candidates']); // Sanitize the ID
+        $delete_sql = "DELETE FROM candidates WHERE name='".$name."'";
+        mysqli_query($con, $delete_sql);
+    }
 
-    mysqli_close($con);
+    if (isset($_POST['btnSearch']) && isset($_POST['search-candidates'])){
+        $name = mysqli_real_escape_string($con, $_POST['search-candidates']);
+        $sql = "SELECT * FROM candidates WHERE name ='".$name."'";
+        $result = mysqli_query($con, $sql);
+    } else {
+        $sql = "SELECT * FROM candidates";
+        $result = mysqli_query($con, $sql);
+    }
+
 ?>
 
 <html>
@@ -27,7 +37,7 @@
             }
 
             table, tr, th {
-                padding: 12px 35px;
+                padding: 12px 20px;
 
             }
 
@@ -43,69 +53,96 @@
                 padding: 12px 20px;
             }
 
+            input[type=submit] {
+                background-color: #007bff;
+                color: #fff;
+                text-decoration: none;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                transition: background-color 0.3s ease;
+            }
+
             .btn {
-            display: inline-block;
-            padding: 10px 20px;
-            background-color: #007bff;
-            color: #fff;
-            text-decoration: none;
-            border: none;
-            border-radius: 5px;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-            margin-top: auto; 
-        }
-        .btn:hover {
-            background-color: #0056b3;
-        }
-    </style>
+                display: inline-block;
+                padding: 10px 20px;
+                background-color: #007bff;
+                color: #fff;
+                text-decoration: none;
+                border: none;
+                border-radius: 5px;
+                cursor: pointer;
+                transition: background-color 0.3s ease;
+                margin-top: auto; 
+            }
+
+            .btn:hover {
+                background-color: #0056b3;
+            }
+        </style>
 
     </head>
     <body>
         <h2><b>Senarai Calon</h2>
-        <input type='search' id='search-candidates' name='search-candidates' placeholder='Cari Calon'/>
-        <input type='submit' value='Cari' id='btnSearch' name='btnSearch'/>
-        <input type='submit' value='Tambah' id='btnAdd' name='btnAdd'/>
-        <br></br>
-        <table align='center'>
-            <tr>
-                <th>No. </th>
-                <th>Nama Penuh</th>
-                <th>No. Kad Pengenalan</th>
-                <th>No. Pendaftaran</th>
-                <th>No. Telefon Bimbit</th>
-                <th>Program</th>
-                <th>Jabatan</th>
-                <th>Tarikh Permohonan</th>
-            </tr>
+        <form method="POST">
+            <input type='search' id='search-candidates' name='search-candidates' placeholder='Cari Calon'/>
+            <input type='submit' value='Cari' id='btnSearch' name='btnSearch'/>
+            <input type='submit' value='Tambah' id='btnAdd' name='btnAdd'/>
+            <input type='submit' value='Padam' id='btnPadam' name='btnPadam'/>
+            <br></br>
+            <table align='center'>
+                <tr>
+                    <th>No. </th>
+                    <th>Nama Penuh</th>
+                    <th>No. Kad Pengenalan</th>
+                    <th>No. Pendaftaran</th>
+                    <th>No. Telefon Bimbit</th>
+                    <th>Program</th>
+                    <th>Jabatan</th>
+                    <th>Ulang Semester</th>
+                    <th>Tindakan Tatatertib</th>
+                    <th>Sedang Tatatertib</th>
+                    <th>Exco MPP</th>
+                    <th>Tarikh Permohonan</th>
+                </tr>
 
-            <?php 
-                // LOOP TILL END OF DATA
-                $i=1;
-                while($rows = mysqli_fetch_array($result))
-                {
-            ?>
-            <tr>
-                <!-- Get data from each column -->
-                <td><?php echo $i;?></td>
-                <td><?php echo $rows['name'];?></td>
-                <td><?php echo $rows['icNo'];?></td>
-                <td><?php echo $rows['regNo'];?></td>
-                <td><?php echo $rows['phoneNo'];?></td>
-                <td><?php echo $rows['program'];?></td>
-                <td><?php echo $rows['jabatan'];?></td>
-                <td><?php echo $rows['date'];?></td>
-            </tr>
-            <?php
+                <?php 
+                if($result){
+                    // LOOP TILL END OF DATA
+                    $i=1;
+                    while($rows = mysqli_fetch_array($result)){
+                ?>
+                <tr>
+                    <!-- Get data from each column -->
+                    <td><?php echo $i;?></td>
+                    <td><?php echo $rows['name'];?></td>
+                    <td><?php echo $rows['icNO'];?></td>
+                    <td><?php echo $rows['regNo'];?></td>
+                    <td><?php echo $rows['phoneNo'];?></td>
+                    <td><?php echo $rows['program'];?></td>
+                    <td><?php echo $rows['jabatan'];?></td>
+                    <td><?php echo $rows['ulang_semester'];?></td>
+                    <td><?php echo $rows['tindakan_tatatertib'];?></td>
+                    <td><?php echo $rows['sedang_tatatertib'];?></td>
+                    <td><?php echo $rows['exco'];?></td>
+                    <td><?php echo $rows['date'];?></td>
+                </tr>
+                <?php
+                    $i++;
+                    }
                 }
-            ?>
-            
-        </table>
-    </body>
-    <body>
+
+                if (isset($_POST['btnAdd'])){
+                    header("location: candidate_form.php");
+                }
+
+                mysqli_close($con);
+                ?>
+            </table>
+        </form>
+
     <div class="container">
         <a href="main_page_officer.php" class="btn">BACK</a>
     </div>
     </body>
-    
 </html>
